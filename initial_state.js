bd8632c2168cf28d6413d1c3d2fce154526bff78
Nicolas Mafra <nickmafra@hotmail.com>
2023-03-14 17:54:45 -0300

    fix gravity

--- initial_state.js
+++ initial_state.js
@@ -1,20 +1,26 @@
 const initialState = [
 	{
-		position: [-10, 0.5, 0],
-		velocity: [5, 0, 0],
+		position: [-10, 0, 0],
+		velocity: [0,5,0],
 		radius: 1,
 		mass: 1
 	},
 	{
-		position: [3,0,0],
+		position: [0,0,0],
 		velocity: [0,0,0],
 		radius: 2,
 		mass: 8
 	},
 	{
-		position: [-3,3,0],
-		velocity: [0,0,0],
+		position: [10,0,0],
+		velocity: [0,-5,0],
 		radius: 1,
 		mass: 1
+	},
+	{
+		position: [-5,-2,0],
+		velocity: [1,-1,0],
+		radius: 0.1,
+		mass: 0.001
 	}
 ]
--- mechanics.js
+++ mechanics.js
@@ -1,31 +1,39 @@
-const elasticConstant = 1000;
-const gravityConstant = 100;
+const gravityConstant = 30;
 
 function change(deltaTime) {
-	
-	// inertia
-	bodies.forEach(x => x.position.addScaledVector(x.velocity, deltaTime));
 	
 	// interactions
 	forEachBodyPair((a, b) => {
 		
 		let diff = b.position.clone().sub(a.position);
-		let dist = diff.length() + 0.1;
+		let dir = diff.clone().normalize();
+		let dist = diff.length();
 		let minDist = a.radius + b.radius;
-		
-		// spring collision
-		if (dist < minDist) {
 			
-			let overlap = minDist - dist;
-			let bForce = diff.clone().setLength(elasticConstant * overlap);
+		let pairVelocity = newVector()
+			.addScaledVector(a.velocity, a.mass)
+			.addScaledVector(b.velocity, b.mass)
+			.divideScalar(a.mass + b.mass);
+		
+		let aDirSpeed = a.velocity.clone().sub(pairVelocity).dot(dir);
+		
+		// elastic collision
+		if (dist < minDist && aDirSpeed > 0) {
+			
+			
+			let bForce = dir.clone().multiplyScalar(2*a.mass*aDirSpeed/deltaTime);
 			
 			applyForcePair(bForce, a, b);
 		}
 		
-		let bGravityForce = diff.clone().setLength(gravityConstant * a.mass * b.mass / (dist * dist));
-		applyForceToBody(bGravityForce, a, b);
+		let gravityDist = dist > minDist ? dist : (minDist + 1/dist);
+		let bGravityForce = diff.clone().setLength(-gravityConstant * a.mass * b.mass / (gravityDist * gravityDist));
+		applyForcePair(bGravityForce, a, b);
 
 	});
+	
+	// inertia
+	bodies.forEach(x => x.position.addScaledVector(x.velocity, deltaTime));
 }
 
 function applyForcePair(bForce, a, b) {
